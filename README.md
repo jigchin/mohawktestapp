52fdff18e2f2ab13e01cd118245880cd55a64857
Jigar Salecha <jigchin@gmail.com>
2022-09-08 10:59:15 +0000

    GIT Initial Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Thu Sep 08 2022 09:49:37 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>@sap/generator-fiori-freestyle|
+|**App Generator Version**<br>1.7.3|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Floorplan Used**<br>2listdetail|
+|**Service Type**<br>OData Url|
+|**Service URL**<br>https://mohawk-carpet--llc-zeql-sandbox-sample-srv.cfapps.eu10.hana.ondemand.com/CPL.xsodata
+|**Module Name**<br>testapp|
+|**Application Title**<br>Master Detail App|
+|**Namespace**<br>|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.102.1|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Object collection**<br>CPLView|
+|**Object collection key**<br>PRODUCT_UNIQUE_KEY__C|
+|**Object ID**<br>ACCOUNT__C|
+|**Object number**<br>BILLING_PRICE__C|
+|**Object unit of measure**<br>PRICING_UOM__C|
+|**Line item collection**<br>Unknown|
+|**Line item collection key**<br>Unknown|
+|**Line item ID**<br>Unknown|
+|**Line item number**<br>Unknown|
+|**Line item unit of measure**<br>Unknown|
+
+## testapp
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,122 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/m/library"
+], function (BaseController, JSONModel, formatter, mobileLibrary) {
+    "use strict";
+
+    // shortcut for sap.m.URLHelper
+    var URLHelper = mobileLibrary.URLHelper;
+
+    return BaseController.extend("testapp.controller.Detail", {
+
+        formatter: formatter,
+
+        /* =========================================================== */
+        /* lifecycle methods                                           */
+        /* =========================================================== */
+
+        onInit: function () {
+            // Model used to manipulate control states. The chosen values make sure,
+            // detail page is busy indication immediately so there is no break in
+            // between the busy indication for loading the view's meta data
+            var oViewModel = new JSONModel({
+                busy : false,
+                delay : 0
+            });
+
+            this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+
+            this.setModel(oViewModel, "detailView");
+
+            this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+        },
+
+        /* =========================================================== */
+        /* event handlers                                              */
+        /* =========================================================== */
+
+        /**
+         * Event handler when the share by E-Mail button has been clicked
+         * @public
+         */
+        onSendEmailPress: function () {
+            var oViewModel = this.getModel("detailView");
+
+            URLHelper.triggerEmail(
+                null,
+                oViewModel.getProperty("/shareSendEmailSubject"),
+                oViewModel.getProperty("/shareSendEmailMessage")
+            );
+        },
+
+        
+
+        /* =========================================================== */
+        /* begin: internal methods                                     */
+        /* =========================================================== */
+
+        /**
+         * Binds the view to the object path and expands the aggregated line items.
+         * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+         * @private
+         */
+        _onObjectMatched: function (oEvent) {
+            var sObjectId =  oEvent.getParameter("arguments").objectId;
+            this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+            var detailModel = new sap.ui.model.json.JSONModel();
+			detailModel.loadData(
+				"/CPL.xsodata/CPLDetailView?$filter=RESIDENTIAL_PRODUCT_UNIQUE_KEY__C eq '"+sObjectId+"'"
+			);
+            this.getView().setModel(detailModel, "detailModel");
+            var oViewModel = this.getModel("detailView");
+            // If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+            oViewModel.setProperty("/busy", false);
+        },
+      
+
+        _onMetadataLoaded: function () {
+            // Store original busy indicator delay for the detail view
+            var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
+                oViewModel = this.getModel("detailView");
+
+            // Make sure busy indicator is displayed immediately when
+            // detail view is displayed for the first time
+            oViewModel.setProperty("/delay", 0);
+
+            // Binding the view will set it to not busy - so the view is always busy if it is not bound
+            oViewModel.setProperty("/busy", true);
+            // Restore original busy indicator delay for the detail view
+            oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
+        },
+
+        /**
+         * Set the full screen mode to false and navigate to list page
+         */
+        onCloseDetailPress: function () {
+            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", false);
+            // No item should be selected on list after detail page is closed
+            this.getOwnerComponent().oListSelector.clearListListSelection();
+            this.getRouter().navTo("list");
+        },
+
+        /**
+         * Toggle between full and non full screen mode.
+         */
+        toggleFullScreen: function () {
+            var bFullScreen = this.getModel("appView").getProperty("/actionButtonsInfo/midColumn/fullScreen");
+            this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", !bFullScreen);
+            if (!bFullScreen) {
+                // store current layout and go full screen
+                this.getModel("appView").setProperty("/previousLayout", this.getModel("appView").getProperty("/layout"));
+                this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
+            } else {
+                // reset to previous layout
+                this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
+            }
+        }
+    });
+
+});
